efb4b0de2dfd3ec68c372a20c1048344162e780a
Felixdavid12 <felixdavidgom@gmail.com>
2021-09-10 20:51:58 -0400

    Añado mi actividad de la Torre Eiffel terminada.
    - Podrían haber conflictos en AndroidManifest.xml, por favor revisar.

---
+++ app/src/main/java/com/lospythones/mobilemisiontic2022/EiffelTowerActivity.kt
@@ -0,0 +1,11 @@
+package com.lospythones.mobilemisiontic2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EiffelTowerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_eiffel_tower)
+    }
+}
