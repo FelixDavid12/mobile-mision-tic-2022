562219940b97bb6c73b00a29410ed2df9223b597
Detrivinog <detrivinog@gmail.com>
2021-09-11 13:41:46 -0500

    Add Pont des Arts Activity and modify strings and resources

---
+++ app/src/main/java/com/lospythones/mobilemisiontic2022/PontDesArtsActivity.kt
@@ -0,0 +1,11 @@
+package com.lospythones.mobilemisiontic2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PontDesArtsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pont_des_arts)
+    }
+}
