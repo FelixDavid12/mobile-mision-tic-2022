f60230b9b88ebe4b127506fde7fe17630da784ec
Your Nameclear <you@example.com>
2021-09-11 07:57:45 -0500

    Activity of the Palace of Versailles finished

---
+++ app/src/main/java/com/lospythones/mobilemisiontic2022/PalaceOfVersaillesActivity.kt
@@ -0,0 +1,11 @@
+package com.lospythones.mobilemisiontic2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PalaceOfVersaillesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_palace_of_versailles)
+    }
+}
