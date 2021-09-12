c68c69104ca0961645faa3471fff785693b30b18
Camilo Builes <ocamilo930@gmail.com>
2021-09-12 14:00:10 -0500

    Auto stash before merge of "main" and "origin/main"
    My first Commit Notre Dame Cathedral

---
+++ app/src/main/java/com/lospythones/mobilemisiontic2022/NotreDameCathedralActivity.kt
@@ -0,0 +1,11 @@
+package com.lospythones.mobilemisiontic2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotreDameCathedralActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activitynotre_dame_cathedral)
+    }
+}
