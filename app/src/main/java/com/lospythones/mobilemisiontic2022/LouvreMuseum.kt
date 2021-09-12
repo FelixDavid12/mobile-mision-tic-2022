0da121d3ff83e15db236f8a7898d359a3c95c409
CamiloPatino <cpatinoj@correo.udistrital.edu.co>
2021-09-12 12:37:09 -0500

    JCP first commit

---
+++ app/src/main/java/com/lospythones/mobilemisiontic2022/LouvreMuseum.kt
@@ -0,0 +1,11 @@
+package com.lospythones.mobilemisiontic2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LouvreMuseum : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_louvre_museum)
+    }
+}
